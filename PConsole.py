23ac55cf3fa8aab18afa1acbabf604aef5d67b67
Daniel Bretschneider <daniel@bretschneider.cc>
2022-03-09 12:21:40 +0100

    ssh commit

---
+++ PConsole.py
@@ -0,0 +1,170 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from pyfiglet import Figlet
+import PConstants
+import readline
+
+
+class PConsole:
+    """
+    Console (CLI) Handler with following functionalities:
+    * processes commands
+    * shows help message
+    """
+
+    def __init__(self):
+        """
+        Constructors
+        """
+        pass
+
+    def welcome_message(self):
+        """
+        Prints welcome message on program start
+        """
+        figlet = Figlet(font='slant')
+        print(figlet.renderText("PassPy"), end='')
+        print(PConstants.VERSION)
+        print(PConstants.COPYRIGHT + PConstants.CLI_NEWLINE)
+
+    def start(self):
+        """
+        Start console loop
+        :return:
+        """
+        while True:
+            command = self.read_command()
+            self.process_command(command)
+
+    def read_command(self):
+        """
+        Get command from user via input and return as string
+        :return:
+        """
+        command = input(PConstants.CLI_PROMPT)
+        return command
+
+    def process_command(self, command):
+        """
+        Process given command
+        :return:
+        """
+        command = command.lower().rstrip().lstrip()
+        splt_command = command.split(" ")
+
+        if command == "":
+            pass
+        elif command == PConstants.CMD_EXIT:
+            exit()
+        elif command == PConstants.CMD_HELP:
+            self.print_help_message()
+        elif command == PConstants.CMD_COUNT:
+            self.print_db_count()
+        elif command == PConstants.CMD_ADD:
+            self.add_entry()
+        elif command == PConstants.CMD_LOGIN:
+            self.login()
+        elif splt_command[0] == PConstants.CMD_HIDE:
+            self.hide_entry()
+        elif splt_command[0] == PConstants.CMD_DELETE:
+            self.delete_entry()
+        elif splt_command[0] == PConstants.CMD_SEARCH:
+            self.search()
+        elif splt_command[0] == PConstants.CMD_SHOW:
+            self.show()
+        else:
+            self.print_message("Command '" + command + "' not found.", 1)
+
+    def print_message(self, text, type):
+        """
+        Print message to terminal
+        :param text: Message Content
+        :param type: (int) INFO (0), ERROR (1)
+        :return:
+        """
+        if type == 1:
+            print(PConstants.CLI_MSG_ERR + text)
+        else:
+            print(PConstants.CLI_MSG_INFO + text)
+
+    def print_help_message(self):
+        """
+        Print help message
+
+        :return:
+        """
+        print("\nCommand | Description")
+        print("-----------------------------------------")
+
+        self.print_help_command("add", "Add new set of credentials")
+        self.print_help_command("login", "Authenticate with Email and Password")
+        self.print_help_command("count", "Count entries in database.")
+        self.print_help_command("delete", "Delete specific entry in database. Needs arguments: ID")
+        self.print_help_command("exit", "Exits the password manager")
+        self.print_help_command("help", "Prints this wonderful message")
+        self.print_help_command("hide", "Hide entry in database. Needs argument: ID")
+        self.print_help_command("reveal", "Reveal password of specific entry. Needs argument: ID")
+        self.print_help_command("search", "Searches for entries. Optional arguments: ID, Title, username, URL")
+        self.print_help_command("show", "Shows specific entry. Needs argument: ID")
+
+        print(PConstants.CLI_NEWLINE)
+
+    def print_help_command(self, command, description):
+        """
+        Line for command
+        :param command: string
+        :return:
+        """
+        print(PConstants.CLI_COL_BOLD + command + PConstants.CLI_COL_END +
+              PConstants.CLI_TAB + description)
+
+    def print_db_count(self):
+        """
+        Prints the number of entries in db
+        :return:
+        """
+
+    def add_entry(self):
+        """
+        Add new entry to db.
+        :return:
+        """
+
+    def login(self):
+        """
+        Login / Authenticate
+        :return:
+        """
+
+    def hide_entry(self):
+        """
+        Hide entry in db
+        :return:
+        """
+
+    def add_entry(self):
+        """
+        Add entry!
+        :return:
+        """
+
+    def delete_entry(self):
+        """
+        Delete entry in db.
+        :return:
+        """
+
+    def search(self):
+        """
+        Implements search function to db
+        (returns more than one result, if found)
+        :return:
+        """
+
+    def show(self):
+        """
+        Implements search function to db
+        (returns only data if search term is correct)
+        :return:
+        """
---
+++ PConstants.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+#
+# GENERAL
+#
+COPYRIGHT = "(C) 2022 Daniel Bretschneider"
+VERSION = "v1.1.3"
+
+#
+# CLI
+#
+CLI_PROMPT = "[passpy] > "
+CLI_MSG_INFO = "[*] "
+CLI_MSG_ERR = "[-] "
+CLI_NEWLINE = "\n"
+CLI_TAB = "\t"
+CLI_COL_END = '\033[0m'
+CLI_COL_BOLD = '\033[1m'
+
+# commands
+CMD_ADD = "add"
+CMD_LOGIN = "login"
+CMD_COUNT = "count"
+CMD_DELETE = "delete"
+CMD_EXIT = "exit"
+CMD_HELP = "help"
+CMD_HIDE = "hide"
+CMD_REVEAL = "reveal"
+CMD_SHOW = "show"
+CMD_SEARCH = "search"
+
+#
+CMD_ATT_ENTRIES = "entries"
+CMD_ATT_ID = "id"
+CMD_ATT_TITLE = "title"
+CMD_ATT_USERNAME = "username"
+CMD_ATT_URL = "url"
